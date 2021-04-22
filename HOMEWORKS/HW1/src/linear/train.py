5d7cd4914fc4607df74872a1fa058f9f023afe5f
Elizaveta55 <32224955+Elizaveta55@users.noreply.github.com>
2021-04-23 00:53:37 +0300

    Update train.py

--- HOMEWORKS/HW1/src/linear/train.py
+++ HOMEWORKS/HW1/src/linear/train.py
@@ -18,7 +18,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu' # get device for trainin
 writer = SummaryWriter()
 
 class noLSTM(torch.nn.Module):
-  def __init__(self, input_size=embedding_size, hidden_layer_size=100, output_size=2):
+  def __init__(self, input_size, hidden_layer_size=100, output_size=2):
         super().__init__()
         self.input_size = input_size
         self.hidden_layer_size = hidden_layer_size
@@ -64,9 +64,9 @@ def f1_loss_one(y_true: torch.Tensor, y_pred: torch.Tensor, is_training=False) -
 def main(args):
 
     #load data
-    train_DataLoader, _  = scripts.get_data(data_path="../data/Data1/train_data.csv",testData = True)
+    train_DataLoader, _, embedding_size  = scripts.get_data(data_path="../data/Data1/train_data.csv",testData = True)
     # train_DataLoader = DataLoader(TensorDataset(train_x, train_y), batch_size=30)
-    model = noLSTM()
+    model = noLSTM(input_size=embedding_size)
     loss_function = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
